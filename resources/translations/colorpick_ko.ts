06b6ce2000df02dca0fd638a3dac123055e21697
JungHee Lee <36289819+MarongHappy@users.noreply.github.com>
2023-08-29 13:09:40 +0900

    Create colorpick_ko.ts

---
+++ resources/translations/colorpick_ko.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>ColorEditor</name>
+    <message>
+        <location filename="../../src/coloreditor.cpp" line="181"/>
+        <source>Inkscape</source>
+        <translation>Inkscape</translation>
+    </message>
+    <message>
+        <location filename="../../src/coloreditor.cpp" line="182"/>
+        <source>Hexa with #</source>
+        <translation>#이 있는 16진수</translation>
+    </message>
+    <message>
+        <location filename="../../src/coloreditor.cpp" line="183"/>
+        <source>Quoted hexa with #</source>
+        <translation>#로 인용된 16진수</translation>
+    </message>
+    <message>
+        <location filename="../../src/coloreditor.cpp" line="184"/>
+        <source>Float values</source>
+        <translation>소수점 값</translation>
+    </message>
+    <message>
+        <location filename="../../src/coloreditor.cpp" line="185"/>
+        <source>Int values</source>
+        <translation>정수 값</translation>
+    </message>
+    <message>
+        <location filename="../../src/coloreditor.cpp" line="186"/>
+        <source>CSS RGB Value</source>
+        <translation>CSS RGB 값</translation>
+    </message>
+</context>
+<context>
+    <name>ContrastPreview</name>
+    <message>
+        <location filename="../../src/contrastpreview.cpp" line="17"/>
+        <source>Contrast Example Text</source>
+        <translation>대비 예시 텍스트</translation>
+    </message>
+    <message>
+        <location filename="../../src/contrastpreview.cpp" line="55"/>
+        <source>Bad</source>
+        <translation>나쁨</translation>
+    </message>
+    <message>
+        <location filename="../../src/contrastpreview.cpp" line="57"/>
+        <source>Acceptable</source>
+        <translation>허용됨</translation>
+    </message>
+    <message>
+        <location filename="../../src/contrastpreview.cpp" line="59"/>
+        <source>Good</source>
+        <translation>좋음</translation>
+    </message>
+    <message>
+        <location filename="../../src/contrastpreview.cpp" line="61"/>
+        <source>Contrast Ratio</source>
+        <translation>명암비</translation>
+    </message>
+</context>
+<context>
+    <name>HsvColorSpace</name>
+    <message>
+        <location filename="../../src/hsvcolorspace.cpp" line="19"/>
+        <source>H</source>
+        <translation>H</translation>
+    </message>
+    <message>
+        <location filename="../../src/hsvcolorspace.cpp" line="20"/>
+        <source>S</source>
+        <translation>S</translation>
+    </message>
+    <message>
+        <location filename="../../src/hsvcolorspace.cpp" line="21"/>
+        <source>V</source>
+        <translation>V</translation>
+    </message>
+</context>
+<context>
+    <name>RgbColorSpace</name>
+    <message>
+        <location filename="../../src/rgbcolorspace.cpp" line="19"/>
+        <source>R</source>
+        <translation>R</translation>
+    </message>
+    <message>
+        <location filename="../../src/rgbcolorspace.cpp" line="20"/>
+        <source>G</source>
+        <translation>G</translation>
+    </message>
+    <message>
+        <location filename="../../src/rgbcolorspace.cpp" line="21"/>
+        <source>B</source>
+        <translation>B</translation>
+    </message>
+</context>
+</TS>
